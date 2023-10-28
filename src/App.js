4fc74580c07fb89091a420b3773929feb86b68dd
Samiriddhi <samriddhi.sreyasee123@gmail.com>
2023-10-28 15:31:15 +0530

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Tyingbox from "./components/Typingbox";
+import Footer from "./components/Footer";
+
+import "./App.css";
+import "./fonts.css";
 
 function App() {
+  const [showLoginPage, setShowLoginPage] = useState(false);
+  const [showSignupPage, setShowSignupPage] = useState(false);
+
+  const handleIconClick = () => {
+    setShowLoginPage(true);
+    setShowSignupPage(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar onUserIconClick={handleIconClick} />
+      {showLoginPage && (
+        <div className="login-overlay">
+          <Login />
+        </div>
+      )}
+      {showSignupPage && (
+        <div className="login-overlay">
+          <Signup />
+        </div>
+      )}
+
+      <Tyingbox />
+      <Footer />
     </div>
   );
 }
